5785d53ef36aeffecb760031829a15fc866436d3
Alina Deeva <d.andreevna.a@gmail.com>
2022-07-11 23:09:05 +0300

    develope

--- src/main/java/PageObjects/OrderPage.java
+++ src/main/java/PageObjects/OrderPage.java
@@ -8,50 +8,47 @@ public class OrderPage {
     private WebDriver driver;
 
     //order field Name .//input[@placeholder='* Имя']
-    private By orderFieldName = By.xpath(".//input[@placeholder='* Имя']");
+    private final By orderFieldName = By.xpath(".//input[@placeholder='* Имя']");
 
     //order field surname .//input[@placeholder='* Фамилия']
-    private By orderFieldSurname = By.xpath(".//input[@placeholder='* Фамилия']");
+    private final By orderFieldSurname = By.xpath(".//input[@placeholder='* Фамилия']");
 
     //order field address .//input[@placeholder='* Адрес: куда привезти заказ']
-    private By orderFieldAddress = By.xpath(".//input[@placeholder='* Адрес: куда привезти заказ']");
+    private final By orderFieldAddress = By.xpath(".//input[@placeholder='* Адрес: куда привезти заказ']");
 
     //order field metro station .select-search__input
-    private By orderFieldMetro = By.className("select-search__input");
+    private final By orderFieldMetro = By.className("select-search__input");
 
     //order field phone number .//input[@placeholder='* Телефон: на него позвонит курьер']
-    private By orderFieldPhone = By.xpath(".//input[@placeholder='* Телефон: на него позвонит курьер']");
+    private final By orderFieldPhone = By.xpath(".//input[@placeholder='* Телефон: на него позвонит курьер']");
 
     //order button Next .Button_Button__ra12g Button_Middle__1CSJM
-    private By orderButtonNext = By.xpath(".//button[@class='Button_Button__ra12g Button_Middle__1CSJM' and text()='Далее']");
+    private final By orderButtonNext = By.xpath(".//button[@class='Button_Button__ra12g Button_Middle__1CSJM' and text()='Далее']");
 
     //field for choose rental start .//input[@placeholder='* Когда привезти самокат']
-    private By RentStartDay = By.xpath(".//input[@placeholder='* Когда привезти самокат']");
+    private final By RentStartDay = By.xpath(".//input[@placeholder='* Когда привезти самокат']");
 
     //field for choose rental period .Dropdown-placeholder
-    private By rentalPeriodField = By.className("Dropdown-placeholder");
+    private final By rentalPeriodField = By.className("Dropdown-placeholder");
 
     //option one with rental period .//div[@class='Dropdown-option' and text()='сутки'
-    private By firstRentalPeriod = By.xpath(".//div[@class='Dropdown-option' and text()='сутки']");
-    private By secondRentalPeriod = By.xpath(".//div[@class='Dropdown-option' and text()='трое суток']");
+    private final By firstRentalPeriod = By.xpath(".//div[@class='Dropdown-option' and text()='сутки']");
+    private final By secondRentalPeriod = By.xpath(".//div[@class='Dropdown-option' and text()='трое суток']");
 
     //colour checkbox black
-    private By colourCheckbox = By.id("black");
+    private final By colourCheckbox = By.id("black");
 
     //comments for couriers .//input[@placeholder='Комментарий для курьера']
-    private By commentForCouriers = By.xpath(".//input[@placeholder='Комментарий для курьера']");
+    private final By commentForCouriers = By.xpath(".//input[@placeholder='Комментарий для курьера']");
 
     //order button .Button_Button__ra12g Button_Middle__1CSJM
-    private By orderButton = By.xpath(".//button[@class='Button_Button__ra12g Button_Middle__1CSJM' and text()='Заказать']");
-
-    //order agree window .Order_Modal__YZ-d3
-    private By agreeWindow = By.className("Order_Modal__YZ-d3");
+    private final By orderButton = By.xpath(".//button[@class='Button_Button__ra12g Button_Middle__1CSJM' and text()='Заказать']");
 
     //yes-button .//button[@class='Button_Button__ra12g Button_Middle__1CSJM' and text()='Да']
-    private By yesButton = By.xpath(".//button[@class='Button_Button__ra12g Button_Middle__1CSJM' and text()='Да']");
+    private final By yesButton = By.xpath(".//button[@class='Button_Button__ra12g Button_Middle__1CSJM' and text()='Да']");
 
     //order window
-    private By orderOkWindow = By.xpath(".//div[@class='Order_ModalHeader__3FDaJ' and text()='Заказ оформлен']");
+    private final By orderIsOkWindow = By.xpath(".//div[@class='Order_ModalHeader__3FDaJ' and text()='Заказ оформлен']");
 
     public OrderPage(WebDriver driver){
         this.driver = driver;
@@ -118,7 +115,7 @@ public class OrderPage {
 
     //get order ok window
     public String getOrderOkWindow() {
-        return driver.findElement(orderOkWindow).getText();
+        return driver.findElement(orderIsOkWindow).getText();
 
     }
 
